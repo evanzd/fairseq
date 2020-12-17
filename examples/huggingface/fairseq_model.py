c001ff0ee2affaf7b5b02fa09b937e4f5cf1b738
Dong Zhou <Zhou.Dong@microsoft.com>
2020-12-17 18:42:08 +0800

    fix

--- examples/huggingface/fairseq_model.py
+++ examples/huggingface/fairseq_model.py
@@ -1,3 +1,6 @@
+import json
+import shutil
+import torch
 import torch.nn as nn
 
 from fairseq import hub_utils
@@ -12,7 +15,7 @@ class RobertaClassifier(nn.Module):
         self.config = config
         self.task = task
         self.model = model
-        self.id2label = id2label
+        self.config.id2label = id2label
         self.fc = nn.Linear(config.encoder_embed_dim, len(id2label))
 
     @staticmethod
@@ -20,12 +23,12 @@ class RobertaClassifier(nn.Module):
         x = hub_utils.from_pretrained(path)
         return RobertaClassifier(x['args'], x['task'], x['models'][0], id2label)
 
-    def save_pretrained(path):
+    def save_pretrained(self, path):
         torch.save({
             'args': self.config,
             'task': self.task,
             'models': [self.model.state_dict()]
-        }, path)
+        }, path + '/model.pt')
 
     def forward(self, input_ids, attention_mask):
         hidden = self.model.encoder.extract_features(input_ids)[0]
@@ -34,6 +37,10 @@ class RobertaClassifier(nn.Module):
 
 
 class RobertaTokenizer:
+
+    def __init__(self, path, tokenizer):
+        self.path = path
+        self.tokenizer = tokenizer
 
     @staticmethod
     def from_pretrained(path, **kwargs):
@@ -44,4 +51,14 @@ class RobertaTokenizer:
         for idx, token in enumerate(['<s>', '<pad>', '</s>', '<unk>']):
             tokenizer.encoder[token] = idx
             tokenizer.decoder[idx] = token
-        return tokenizer
+        return RobertaTokenizer(path, tokenizer)
+
+    def save_pretrained(self, path):
+        shutil.copyfile(self.path + '/encoder.json', path + '/encoder.json')
+        shutil.copyfile(self.path + '/vocab.bpe', path + '/vocab.bpe')
+
+    def __call__(self, *args, **kwargs):
+        return self.tokenizer(*args, **kwargs)
+
+    def __getattr__(self, key):
+        return getattr(self.tokenizer, key)
